447db9b98fe697f93c7212a7af02c31d5cb25985
Nik0dem0-py <81317046+Nik0dem0-py@users.noreply.github.com>
2021-11-19 19:32:00 +0000

    moderation commands :smile:

---
+++ cogs/moderation.py
@@ -0,0 +1,51 @@
+import nextcord
+from nextcord.ext import commands
+from nextcord.errors import Forbidden
+
+class Moderation(commands.Cog):
+    """
+    Moderation commands.
+    """
+        
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.Cog.listener()
+    async def on_ready(self):
+        print('Cog "Moderation" has been loaded.')
+
+    @commands.command()
+    @commands.has_permissions(kick_members=True)
+    async def kick(self, ctx, member: commands.MemberConverter):
+        """Kicks the following member"""
+        await ctx.send('What is the reason?')
+        msg = await self.bot.wait_for('message')
+        reason = msg.content
+        description = f'''
+        **Member:** = {member}
+        **Responsible moderator:** {ctx.author.mention}
+        **Reason:** {reason}
+        '''
+
+        embed = nextcord.Embed(title='Kick', description=description, colour=nextcord.Colour.green())
+        try:
+            await member.kick(reason=reason)
+            await ctx.send(content=None, embed=embed)
+        except Forbidden:
+            try:
+                await ctx.send("Hey, seems like I can't kick people. Please check my permissions :)")
+            except Forbidden:
+                await ctx.author.send(
+                    f"Hey, seems like I can't send any message in {ctx.channel.name} on {ctx.guild.name}\n"
+                    f"May you inform the server team about this issue? :slight_smile: ", embed=embed)
+
+
+
+
+
+
+
+
+
+def setup(bot):
+    bot.add_cog(Moderation(bot))
