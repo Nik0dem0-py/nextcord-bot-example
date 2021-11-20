c2c61389b223e30d34a73d3d7f2ff7746e4ac5d2
NikoUwU <81317046+Nik0dem0-py@users.noreply.github.com>
2021-11-20 15:38:04 +0100

    yes

--- cogs/utilities.py
+++ cogs/utilities.py
@@ -19,5 +19,12 @@ class Utilities(commands.Cog):
         """Sends bot latency :ping_pong:"""
         await ctx.send(f'Pong! :ping_pong: `{round(self.bot.latency * 1000)}ms`')
 
+    @commands.command()
+    @commands.has_permissions(manage_channels=True)
+    async def setdelay(self, ctx, seconds: int):
+        """Sets slowmode in current channel"""
+        await ctx.channel.edit(slowmode_delay=seconds)
+        await ctx.send(f"Set the slowmode delay in this channel to {seconds} seconds!")
+
 def setup(bot):
     bot.add_cog(Utilities(bot))
