5fa09d2e3a585194da45e896e649c02a05680f38
Nik0dem0-py <81317046+Nik0dem0-py@users.noreply.github.com>
2021-12-21 10:03:02 +0100

    Update config.py

--- config.py
+++ config.py
@@ -12,19 +12,9 @@ BOT_NAME= "Example Nextcord (GitHub)"
 
 DEVELOPER_ROLE_ID= "811902358157131806"
 CONTENT_CREATOR_ROLE_ID= "921787921747550249"
-<<<<<<< HEAD
 YOUTUBE_PING_ROLE_ID= "921787845784502302"
 MINECRAFT_PING_ROLE_ID= "816989544623898665"
 
 #Guild IDs
 
-GUILD_IDS=[811178517969895474]
 
-
-def custom_id(view: str, id: int) -> str:
-    """create a custom id from the bot name : the view : the identifier"""
-    return f"{BOT_NAME}:{view}:{id}"
-
-=======
-YOUTUBE_PING_ROLE_ID= "921787845784502302"
->>>>>>> parent of 43e1aec (slash commands :eyes:)
